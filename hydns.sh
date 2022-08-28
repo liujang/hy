649d5815df30fc01ad458d241ba25772ab508447
liujang <41243924+liujang@users.noreply.github.com>
2022-08-28 21:44:35 +0800

    Update hydns.sh

--- hydns.sh
+++ hydns.sh
@@ -6,43 +6,6 @@ BLUE="\033[0;36m"
 FUCHSIA="\033[0;35m"
 echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
-if [[ "$EUID" -ne 0 ]]; then
-    echo "false"
-  else
-    echo "true"
-  fi
-if [[ -f /etc/redhat-release ]]; then
-		release="centos"
-	elif cat /etc/issue | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-	elif cat /proc/version | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /proc/version | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-    fi
-    
-     if [[ $release = "ubuntu" || $release = "debian" ]]; then
-    PM='apt'
-  elif [[ $release = "centos" ]]; then
-    PM='yum'
-  else
-    exit 1
-  fi
-  # PM='apt'
-  if [ $PM = 'apt' ] ; then
-    apt-get install -y cron
-    service cron start
-elif [ $PM = 'yum' ]; then 
-    yum install -y vixie-cron
-    yum install -y crontabs
-    service cron start
-fi
 echo -e "
  ${GREEN} hk
  ${GREEN} jp
@@ -61,11 +24,6 @@ wget -N --no-check-certificate "https://raw.githubusercontent.com/liujang/hy/mai
 sed -i '9s/area/'${area}'/' /root/changedns.sh
  ./changedns.sh
  echo "已更换dns"
-crontab -l > conf
-echo "0 */1 * * * /root/changedns.sh" >> conf
-crontab conf
-rm -f conf
-echo "已设置每小时重新获取dns"
 else
 echo "网站访问状态为${num},不可以执行脚本，已自动退出"
 exit 1
